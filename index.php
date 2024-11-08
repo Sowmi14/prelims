5440c5c22a2fcc4be496b53918530e79a0a011c1
christian <dr1ch1416@gmail.com>
2024-11-09 02:37:57 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Bootstrap CSS CDN -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQdX0aLoJgY+OU9cXw7S1Vu+zj9XGhdxxH/j6GcY3izcCJK2ftR5McK5cg" crossorigin="anonymous">
+</head>
+<body>
+  <div class="container text-center mt-5">
+    <h1 class="display-4">Hello, World!</h1>
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeo9F33e9Yv43KYsnz6T8z4p3yYYjFnvZF0PSFzhK5q6pjl6" crossorigin="anonymous"></script>
+</body>
+</html>
